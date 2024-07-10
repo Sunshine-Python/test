1920ac785934477a2dd1101483b797508f5b1d6c
Sunshine-Python <carena.wilson@outlook.com>
2024-07-10 13:38:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ from strategies.standard_deviation import StdDevStrategy, std_dev_viz, run_std_d
 st.set_page_config(layout="wide", page_title="Little John")
 
 # Function to load CSS file
-def load_css("style.css"):
-    with open("style.css") as f:
+def load_css(style.css):
+    with open(style.css) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Load CSS
